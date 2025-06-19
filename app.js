1a49093432f4224bc6164a38eb2467d0d21464dc
Muhammad Furqan <muhammadfurqanch517@gmail.com>
2025-06-19 11:18:32 +0500

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000", "https://raasid-x-go.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
